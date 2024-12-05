b8b6d899a874ab4f5de626544df05a243406fbb1
Himesh-Kundal <infernoemperor@proton.me>
2024-12-05 22:33:23 +0530

    LessonDataCodeTests and LessonDataCodeCompletion

---
+++ Ch 1 Variables/Type Sizes/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	accountAgeFloat := 2.6
+
+	fmt.Println("Your account has existed for", accountAgeInt, "years")
+}
---
+++ Ch 9 Maps/Suggested Friends/main.go
@@ -0,0 +1,5 @@
+package main
+
+func findSuggestedFriends(username string, friendships map[string][]string) []string {
+	// ?
+}
